a826a0dc1332d3994eb30e66f176b54240fcee58
bojoluis21798 <trinity21798@gmail.com>
2020-07-20 23:09:43 +0800

    Change to grids

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,14 +1,13 @@
 import React, {Fragment} from "react";
 import {useStore} from "../../store";
 import {observer} from "mobx-react";
-import {Category} from "./styles";
-import {Flex, JustifyContent, AlignItems} from "../Flex";
+import {Category, Grid} from "./styles";
 
 export const Categories = observer(() => {
     const {store} = useStore();
 
     return(
-        <Flex justify={JustifyContent.CENTER} alignItems={AlignItems.CENTER}>
+        <Grid>
             {
                 store.categories.length === 0 && <p>Loading..</p>
             }
@@ -18,6 +17,6 @@ export const Categories = observer(() => {
                 <Category key={category.id}>{category.name}</Category>
             )}
             </Fragment>
-        </Flex>
+        </Grid>
     );
 })
--- src/components/Categories/styles.ts
+++ src/components/Categories/styles.ts
@@ -3,9 +3,13 @@ import styled from "styled-components";
 export const Category = styled.div`
     border:1px solid black;
     cursor: pointer;
-    min-width: 12em;
     font-size: 2.5em;
     padding: 0.5em;
     margin: 1em;
     text-align: center;
 `; 
+
+export const Grid = styled.div`
+    display: grid;
+    grid-auto-columns: minmax(max-content, 2fr);
+`;

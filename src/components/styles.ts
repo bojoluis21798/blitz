1759c102ff3dec7fae6167c9f12883d441e7df0e
bojoluis21798 <trinity21798@gmail.com>
2020-07-26 23:28:14 +0800

    add absolute position prop

--- src/components/styles.ts
+++ src/components/styles.ts
@@ -6,9 +6,16 @@ const LoadingAnim = keyframes`
     }
 `;
 
-export const Loading = styled.div`
+interface LoadingPosition{
+    top?: string;
+    left?:string;
+}
+export const Loading = styled.div<LoadingPosition>`
+    ${props=>(props.top && props.left) && "position:absolute"};
+    ${props=>props.top && "top:"+props.top};
+    ${props=>props.left && "left:"+props.left};
     border-radius: 50%;
-    border: 1px solid black;
+    border: 1px solid #25185e;
     width: 10em;
     height: 10em;
     border-bottom: 1px solid white;

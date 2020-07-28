5f27d818d34e97af993f89e29b0359ea14c4a715
bojoluis21798 <trinity21798@gmail.com>
2020-07-28 20:33:09 +0800

    Added Scoreboard

--- src/components/Quiz/index.tsx
+++ src/components/Quiz/index.tsx
@@ -4,7 +4,7 @@ import {withRouter} from "react-router";
 import * as Styled from "./styles";
 import {Loading} from "../styles";
 import {useLocalStore, useObserver} from "mobx-react";
-import {when} from "mobx";
+import {reaction} from "mobx";
 import {useStore} from "../../store";
 import parse from "html-react-parser";
 import {Textfit} from "react-textfit";
@@ -18,10 +18,15 @@ interface IQuestion{
     type:string;
 }
 
-export enum EDifficulty {
-    EASY = "Easy",
-    MEDIUM = "Medium",
-    HARD = "Hard"
+export enum AnswerEval {
+    CORRECT = "correct",
+    WRONG = "wrong",
+}
+
+interface IChoice {
+    eval: AnswerEval;
+    text: string;
+    isClicked: boolean;
 }
 
 interface IQuizStore {
@@ -30,46 +35,24 @@ interface IQuizStore {
     currentQuestion:string;
     category: string;
     timeRemaining: number;
-    difficulty:EDifficulty;
     hasLoaded:boolean;
     hasFailed:boolean;
     hasCompleted:boolean;
-    score:number;
-    currentChoices:string[];
-    correctCount: {
-        easy: number;
-        medium: number;
-        hard: number;
-    },
-    addScore(num:number):void;
+    currentChoices:IChoice[];
+    correctCount: number,
     next():void;
-    evaluateAnswer(choice:string):AnswerEval;
-    startTimer():void;
     resetTimer():void;
     pauseTimer():void;
-}
-
-export enum AnswerEval {
-    CORRECT = "correct",
-    WRONG = "wrong",
 }
 
 // question length
 const easyLength = 10;
 const mediumLength = 7;
 const hardLength = 3;
-// points added for each difficulty
-const easyPoints = 1;
-const mediumPoints = 3;
-const hardPoints = 5;
 // wait time in before transitioning to the next question
-const waitTime = 500;
+const waitTime = 1000;
 // Time given to answer question
 const timeRemaining = 15;
-// coin rewards for each correct answer per difficulty
-const easyReward = 20;
-const mediumReward = 30;
-const hardReward = 50; 
 
 //Timeout IDs
 let timerID:NodeJS.Timeout = null;
@@ -77,13 +60,8 @@ let waitID:NodeJS.Timeout = null;
 //let completedWaitID:NodeJS.Timeout = null;
 
 export const Quiz = withRouter(({history}) => {
-    //const {id} = useParams();
-    const id = 9;
+    const {id} = useParams();
     const {store} = useStore();      
-    
-    const buttonRef:React.RefObject<HTMLButtonElement>[] = [
-        useRef(null), useRef(null), useRef(null), useRef(null)
-    ];
 
     const quizStore = useLocalStore<IQuizStore>(()=>({
         questions: [],
@@ -92,48 +70,42 @@ export const Quiz = withRouter(({history}) => {
         hasFailed: false,
         timeRemaining: timeRemaining,
         hasLoaded: false,
-        score: 0,
-        correctCount: {
-            easy: 0,
-            medium: 0,
-            hard: 0,
-        },
+        correctCount: 0,
         get currentQuestion(){ 
             return this.questions[this.index].question;
         },
         get currentChoices(){
-            let choices:string[] = this.questions[this.index].incorrect_answers.slice(); 
-            choices.splice(Math.floor(Math.random()*10)%4, 0, this.questions[this.index].correct_answer);
+            let choices:IChoice[] = [];
+
+            this.questions[this.index].incorrect_answers.forEach(answer=>choices.push({
+                text: answer,
+                eval:AnswerEval.WRONG,
+                isClicked: false
+            }));
+
+            choices.splice(Math.floor(Math.random()*10)%4, 0, 
+                {
+                    text: this.questions[this.index].correct_answer,
+                    eval: AnswerEval.CORRECT,
+                    isClicked: false
+                });
             return choices;
-        },
-        get difficulty(){
-            const difficulty = this.questions[this.index].difficulty;
-            switch(difficulty){
-                case "easy":
-                    return EDifficulty.EASY;
-                case "medium":
-                    return EDifficulty.MEDIUM;
-                case "hard":
-                    return EDifficulty.HARD;
-            }
         },
         get category(){
             return this.questions[this.index].category;
-        },
-        addScore(num){
-            this.score += num;
         },
         next(){
             if(this.index+1 < this.questions.length){
                 this.index++;
             }else{
                 this.hasCompleted = true;
+                clearInterval(timerID);
             }
         },
-        evaluateAnswer(choice){
-            return (choice === this.questions[this.index].correct_answer)? AnswerEval.CORRECT: AnswerEval.WRONG;
-        },
-        startTimer(){
+        resetTimer(){
+            clearInterval(timerID);
+            this.timeRemaining = timeRemaining;
+
             timerID = setInterval(()=>{
                 if(this.timeRemaining > 0){
                     this.timeRemaining--;
@@ -143,12 +115,6 @@ export const Quiz = withRouter(({history}) => {
                 }
             },1000);
         },
-        resetTimer(){
-            clearInterval(timerID);
-            this.timeRemaining = timeRemaining;
-
-            this.startTimer();
-        },
         pauseTimer(){
             clearInterval(timerID);
         }
@@ -156,58 +122,41 @@ export const Quiz = withRouter(({history}) => {
 
     const [reveal, setReveal] = useState<boolean>(false);
     const [hasFetchError, setHasFetchError] = useState<boolean>(false);
-    //const [shouldRedirect, setShouldRedirect] = useState<boolean>(false);
+
+    const fetchResponses = async () => {
+        const settings = [
+            {
+                difficulty:"easy",
+                length: easyLength
+            }, {
+                difficulty:"medium",
+                length: mediumLength
+            }, {
+                difficulty: "hard",
+                length: hardLength
+            }];
+
+        let resultsMap = settings.map(async (setting)=>{
+            try{
+                let response = await fetch(`https://opentdb.com/api.php?amount=${setting.length}&category=${id}&difficulty=${setting.difficulty}&type=multiple`);
+                let data = await response.json();
+                return data.results;
+            }catch(e){
+                setHasFetchError(true)
+            };
+        })
+
+        let results = await Promise.all(resultsMap);
+
+        results.forEach((result)=>{
+            quizStore.questions = quizStore.questions.concat(result);
+        });
+
+        quizStore.hasLoaded = true;
+        quizStore.resetTimer();
+    }
 
     useEffect(()=>{
-        /*when(
-            ()=>quizStore.hasLoaded,
-            ()=>quizStore.startTimer()
-        );*/
-
-        when(
-            ()=>quizStore.hasCompleted || quizStore.hasFailed,
-            ()=>{
-                clearInterval(timerID);
-                /*completedWaitID = setTimeout(()=>{
-                    setShouldRedirect(true);
-                },1000)*/
-            }
-        );
-    }, []);
-
-    useEffect(()=>{
-        const fetchResponses = async () => {
-            const settings = [
-                {
-                    difficulty:"easy",
-                    length: easyLength
-                }, {
-                    difficulty:"medium",
-                    length: mediumLength
-                }, {
-                    difficulty: "hard",
-                    length: hardLength
-                }];
-
-            let resultsMap = settings.map(async (setting)=>{
-                try{
-                    let response = await fetch(`https://opentdb.com/api.php?amount=${setting.length}&category=${id}&difficulty=${setting.difficulty}&type=multiple`);
-                    let data = await response.json();
-                    return data.results;
-                }catch(e){
-                    setHasFetchError(true)
-                };
-            })
-
-            let results = await Promise.all(resultsMap);
-
-            results.forEach((result)=>{
-                quizStore.questions = quizStore.questions.concat(result);
-            });
-
-            quizStore.hasLoaded = true;
-        }
-
         fetchResponses();
     },[]);
 
@@ -219,6 +168,14 @@ export const Quiz = withRouter(({history}) => {
         }
     },[]);
 
+    const handleRetryClick = ()=>{
+        quizStore.questions = [];
+        quizStore.hasCompleted = false;
+        quizStore.hasFailed = false;
+        quizStore.hasLoaded = false;
+        fetchResponses();
+    }
+
     const handleBackClick = () => {
         history.push({
             pathname: "/",
@@ -226,86 +183,84 @@ export const Quiz = withRouter(({history}) => {
         })
     }
 
-    const handleChoiceClick = (ref:React.RefObject<HTMLButtonElement>, choice:string, difficulty:EDifficulty)=>{
+    const handleChoiceClick = (choice:IChoice)=>{
         quizStore.pauseTimer();
         
-        if(quizStore.evaluateAnswer(choice)===AnswerEval.CORRECT){
-            ref.current.style.backgroundColor = "green";
-            switch(difficulty){
-                case EDifficulty.EASY:
-                    quizStore.correctCount.easy++;
-                    quizStore.addScore(easyPoints);
-                    break;
-                case EDifficulty.MEDIUM:
-                    quizStore.correctCount.medium++;
-                    quizStore.addScore(mediumPoints);
-                    break;
-                case EDifficulty.HARD:
-                    quizStore.correctCount.hard++;
-                    quizStore.addScore(hardPoints);
-                    break;
-            }
-        }else{
-            ref.current.style.backgroundColor = "red";
-            ref.current.style.color = "white";
+        choice.isClicked = true;
+        if(choice.eval === AnswerEval.CORRECT){
+            quizStore.correctCount++;
         }
 
         setReveal(true);
         waitID = setTimeout(()=>{
             quizStore.next();
             quizStore.resetTimer();
-            ref.current.style.backgroundColor = "";
-            ref.current.style.color = "";
+            choice.isClicked = false;
             setReveal(false);
         },waitTime)
     }
 
     return useObserver(()=>(
-        <Styled.Container loaded={quizStore.hasLoaded}>    
-            {!quizStore.hasLoaded && <Loading/>}
-
+        <>
+            {!quizStore.hasLoaded && 
+                <Styled.Container justify="center">  
+                    <Loading/>
+                </Styled.Container>
+            }
             {
                 (quizStore.hasLoaded && !(quizStore.hasFailed || quizStore.hasCompleted)) &&
-                <>
-                    <Styled.Header>
-                        <Styled.Back onClick={handleBackClick}></Styled.Back>
-                        <Styled.TopInfo>
-                            <Styled.Category>{quizStore.category}</Styled.Category>
-                        </Styled.TopInfo>
-                    </Styled.Header>
-                    <Styled.Question>
-                        <Textfit style={{height:'100%', width:"100%", display:"flex", alignItems:"center"}}>
-                            {parse(quizStore.currentQuestion)}
-                        </Textfit>
-                    </Styled.Question>
+                <Styled.Container justify="space-between">
+                    <Styled.HeadBack>
+                        <Styled.Header>
+                            <Styled.Back onClick={handleBackClick}>
+                                <Styled.BackImage/>
+                            </Styled.Back>
+                            <Styled.TopInfo>
+                                <Styled.Category>{quizStore.category}</Styled.Category>
+                            </Styled.TopInfo>
+                        </Styled.Header>
+                        <Styled.Question>
+                            <Textfit style={{height:'100%', width:"100%", display:"flex", alignItems:"center"}}>
+                                {parse(quizStore.currentQuestion)}
+                            </Textfit>
+                        </Styled.Question>
+                    </Styled.HeadBack>
                     <Styled.ChoicesContainer>   
                         {quizStore.currentChoices.map((choice,index)=>
                             <Styled.Choices
                                     key={index}
-                                    eval={quizStore.evaluateAnswer(choice)}
+                                    eval={choice.eval}
+                                    isClicked={choice.isClicked}
                                     reveal={reveal}
-                                    ref={buttonRef[index]}
-                                    onClick={(e)=>handleChoiceClick(buttonRef[index], choice, quizStore.difficulty)}
+                                    onClick={(e)=>handleChoiceClick(choice)}
                                     disabled={reveal}
                             >  
-                                {parse(choice)}
+                                {parse(choice.text)}
                             </Styled.Choices>
                         )}
                     </Styled.ChoicesContainer>
-                    <Styled.Timer>{quizStore.timeRemaining}</Styled.Timer>
-                </>
+                    <Styled.Timer timeRemaining={quizStore.timeRemaining} timeTotal={timeRemaining}></Styled.Timer>
+                </Styled.Container>
             }
-
             {
                 (quizStore.hasCompleted || quizStore.hasFailed) &&
-                <>
-                    <h1>Your Final Score: {quizStore.score}</h1>
-                    <h1>Easy: {quizStore.correctCount.easy} x {easyPoints}</h1>
-                    <h1>Medium: {quizStore.correctCount.medium} x {mediumPoints}</h1>
-                    <h1>Hard: {quizStore.correctCount.hard} x {hardPoints}</h1>
-                    {quizStore.hasCompleted ? <h1>Completed</h1>:<h1>Failed</h1>}
-                </>
+                <Styled.Container 
+                    justify="space-around"
+                    color={quizStore.hasCompleted ? "green":"red"}>
+                    <Styled.StatusFinal>
+                        {quizStore.hasCompleted ? "Success!": "Failed!"}
+                    </Styled.StatusFinal>
+                    <Styled.FinalScoreDisplay>
+                        <Styled.FinalScoreText>
+                            {quizStore.correctCount}/{quizStore.questions.length}
+                        </Styled.FinalScoreText>
+                    </Styled.FinalScoreDisplay>
+                    <Styled.OptionsContainer>
+                        <Styled.OptionsButtons onClick={handleRetryClick}>Retry</Styled.OptionsButtons>
+                        <Styled.OptionsButtons onClick={handleBackClick}>Select Category</Styled.OptionsButtons>
+                    </Styled.OptionsContainer>
+                </Styled.Container>
             }  
-        </Styled.Container>
+        </>
     ));
 });
--- src/components/Quiz/styles.ts
+++ src/components/Quiz/styles.ts
@@ -1,18 +1,84 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 import {AnswerEval} from "./index";
 
 const border = `border: 1px solid black;`;
 
 interface ContainerProps {
-    loaded:boolean;
+    justify: string;
+    color?: string;
 }
 export const Container = styled.div<ContainerProps>`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: ${props=>props.loaded ? 'space-between': 'center'};
-    background-color:#000121;
+    justify-content: ${props=>props.justify};
+    background-color: ${props=>props.color || 'white'};
     height:100%;
+    overflow-x:hidden;
+`;
+
+export const StatusFinal = styled.span`
+    @media(max-width: 1024px){
+        font-size: 20vw;
+        margin: 5vw;
+    }
+    font-size: 10vw;
+    color:white;
+    font-weight: 900;
+`; 
+
+export const OptionsButtons = styled.button`
+    border-radius: 2vw;
+    @media(max-width: 1024px){
+        margin: 1vw;
+        heigth: 45vw;
+        width: 45vw;
+        padding: 2vw;
+        font-size: 8vw;
+    }
+    box-shadow: 3px 3px 10px black;
+    font-size: 3vw;
+    margin: 1vw;
+    heigth: 15vw;
+    width: 30vw;
+    padding: 2vw;
+    background-color: white;
+    outline:none;
+    border:none;
+`;
+
+export const OptionsContainer = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: space-around;
+`;
+
+export const FinalScoreDisplay = styled.div`
+    border-radius: 50%;
+    background-color:white;
+    box-shadow: 3px 3px 10px black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    @media(max-width:1024px){
+        width: 50vw;
+        height: 50vw;
+        margin: 2vw;
+        font-size: 30vw;
+    }
+    width: 20vw;
+    height: 20vw;
+    font-size: 1vw;
+`;
+
+export const FinalScoreText = styled.span`
+    font-weight: 400;
+    @media(max-width:1024px){
+        font-size: 10vw;
+    }
+    font-size: 7vw;
 `;
 
 export const Question = styled.div`
@@ -35,8 +101,24 @@ export const Difficulty = styled.div`
     font-weight: 100;
 `;
 
-export const Timer = styled.div`
-    ${border}
+interface TimerProps{
+    timeRemaining: number;
+    timeTotal: number;
+}
+export const Timer = styled.div<TimerProps>`
+    height: 2vw;
+    border-radius: 2vw;
+    width: ${props=>(props.timeRemaining)/(props.timeTotal)*100}%;
+    background-color: ${props => {
+        if((props.timeRemaining)/(props.timeTotal) > 0.5){
+            return "#00c21d";
+        }else if((props.timeRemaining)/(props.timeTotal) > 0.2){
+            return "#c27b00";
+        }else{
+            return "#c20600";
+        }
+    }};
+    transition: 1000ms linear;
 `;
 
 export const Score = styled.div`
@@ -47,6 +129,7 @@ interface ChoicesProps {
     eval:AnswerEval;
     reveal:boolean;
     disabled:boolean;
+    isClicked: boolean;
 }
 
 export const ChoicesContainer = styled.div`
@@ -57,7 +140,15 @@ export const ChoicesContainer = styled.div`
         grid-gap: 1vh;
         grid-template-columns:1fr;
     }
-    padding-bottom: 1em;
+`;
+
+export const HeadBack = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #000121;
+    width:100%;
 `;
 
 export const Header = styled.div`
@@ -81,8 +172,6 @@ export const Category = styled.span`
 `;
 
 export const Back = styled.div`
-    background-image: url('${require('../../assets/back.png').default}');
-    background-size: 100% 100%;
     @media(max-width:1024px){
         height: 10vw;
         width: 10vw;
@@ -91,6 +180,7 @@ export const Back = styled.div`
     width: 4vw;
     position:absolute;
     left:1%;
+    cursor: pointer;
 `;
 
 export const BackImage = styled.img.attrs(()=>({
@@ -109,13 +199,13 @@ export const TopInfo = styled.div`
 `;
 
 export const Choices = styled.button<ChoicesProps>`
-    border-radius: 1em;
+    border-radius: 2vw;
     border:none;
     outline:none;
     @media(max-width:1024px){
         width: 85vw;
         height: 25vw;
-        font-size: 6vw;
+        font-size: 5vw;
     }
     box-shadow: 2px 2px 10px black;
     display: flex;
@@ -123,9 +213,16 @@ export const Choices = styled.button<ChoicesProps>`
     align-items:center;
     height: 7.5vw;
     width: 44vw;
-    font-size: 4vw;
+    font-size: 3vw;
     text-align:center;
     font-weight: 100;
-    color: ${props=>props.eval === AnswerEval.CORRECT && props.reveal ? "white": "black"};
-    background-color: ${props=>props.eval === AnswerEval.CORRECT && props.reveal ? `green`:`white`};
+    color:black;
+    ${props=>{
+        if(props.eval === AnswerEval.CORRECT && props.reveal) 
+            return `background-color:green; color:white;`;
+        else if(props.eval === AnswerEval.WRONG && props.isClicked && props.reveal)
+            return 'background-color:red; color:white';
+        else 
+            return `white`;
+    }};
 `;
